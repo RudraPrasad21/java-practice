aad1a88ec9880a9e8667b790dea42793a7ed5f38
Rudra Prasad Pujari <rudraprasad.omm21@gmail.com>
2025-06-04 01:27:24 +0530

    check_through_keyboards

---
+++ check through keyboard/check_keyboards.java
@@ -0,0 +1,14 @@
+
+import java.util.Scanner;
+
+
+class xyz
+{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String name;
+        System.out.println("give your name");
+        name = sc.nextLine();
+        System.out.println("hi "+name);
+    }
+}

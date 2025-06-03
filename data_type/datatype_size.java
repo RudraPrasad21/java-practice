7cc1216f181c43f0ad0cb21a8c114fb21383d228
Rudra Prasad Pujari <rudraprasad.omm21@gmail.com>
2025-06-04 01:50:29 +0530

    datatype

---
+++ data_type/datatype_size.java
@@ -0,0 +1,10 @@
+class datatype {
+    public static void main(String[] args) {
+        System.out.println("int max: "+ Integer.MIN_VALUE);
+        System.out.println("int max: "+ Integer.MAX_VALUE);
+        System.out.println("int max: "+ Integer.BYTES);
+        System.out.println("int max: "+ Integer.TYPE);
+        System.out.println("int max: "+ float.class);
+
+    }
+}

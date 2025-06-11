963f62bf9cbdb0c15489f3f54c80c14cda5c9e1a
Rudra Prasad Pujari <rudraprasad.omm21@gmail.com>
2025-06-12 00:39:13 +0530

    arithematic

---
+++ arithematic/arithematic.java
@@ -0,0 +1,20 @@
+
+public class arithematic {
+    public static void main(String... args)
+    {
+        int a=14;
+        int b=5;
+        float c= 2.5f;
+        float d= 3.5f;
+
+        float x=(float)a/b;
+        int y=a%b;
+        float z= c%d;
+        float z1=(float)(a+b)/2;
+
+        System.out.println(x);
+        System.out.println(y);
+        System.out.println(z);
+        System.out.println(z1);
+    }
+}
